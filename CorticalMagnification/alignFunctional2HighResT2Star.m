a533ed6a4c75220a1bec18904e22475065086a24
Julien Besle <julienbesle@gmail.com>
2016-11-24 13:00:32 +0000

    alignFunctional2HighResT2Star.m: problem with applying the alignment was due to FLIRT xform matrix being in world space (mm)

--- CorticalMagnification/alignFunctional2HighResT2Star.m
+++ CorticalMagnification/alignFunctional2HighResT2Star.m
@@ -1,10 +1,12 @@
-function alignFunctional2HighResT2Star(xformFile,HighResT2Star,filenames)
-% We assume the s-form in this header transforms from high res t2Star to whole head space
-T2StarHDR = mlrImageReadNiftiHeader(HighResT2Star);
-% shift0p5 = [1 0 0 0.5; 0 1 0 0.5; 0 0 1 0; 0 0 0 1];
+function alignFunctional2HighResT2Star(xformFile,t2Star,filenames)
 
-% xform = transformation from the fMRI to the low resolution (same as fMRI)
-% high res t 2 star - This is why we have to take into account the difference in in-plane voxel size
+% We assume the s-form in this header transforms from (high res) t2Star to whole head space
+t2Star = mlrImageReadNiftiHeader(t2Star);
+t2starVoxelSizeXform = [t2Star.pixdim(2) 0 0 0; 0 t2Star.pixdim(3) 0 0;0 0 t2Star.pixdim(4) 0; 0 0 0 1];
+
+% xform = transformation from the fMRI to the low resolution (same as fMRI) T2star 
+% this is a transformation matrix output by FLIRT, so in world space (mm)
+% we will have to take into account the voxel size
 xform = textread(xformFile);
 xform = xform(1:4,1:4);
 if ieNotDefined('filenames')
@@ -14,21 +16,18 @@ end
 if ~iscell(filenames)
     filenames = {filenames};
 end
+
 for p = 1:length(filenames)
     hdr = mlrImageReadNiftiHeader(filenames{p});
-    voxelSizeRatio = T2StarHDR.dim(2:3)./hdr.dim(2:3);
-    fMRI2highResT2Star = [voxelSizeRatio(1) 0 0 0; 0 voxelSizeRatio(2) 0 0;0 0 1 0; 0 0 0 1];
+    fMRIvoxelSizeXform = [hdr.pixdim(2) 0 0 0; 0 hdr.pixdim(3) 0 0;0 0 hdr.pixdim(4) 0; 0 0 0 1];
     
-    % compose transform matrix
-    % Transform from fMRI to low res T2 star (xform), shift voxel index to centre (shift0p5), then convert from low
-    % resolution to high resolution voxel size (fMRI2highResT2Star), shift
-    % back voxel index, final, apply transformation from high res t2 star to whole head
-    %     hdr = cbiSetNiftiSform(hdr,T2StarHDR.sform44 * inv(shift0p5) * fMRI2highResT2Star * shift0p5 * xform);
-    
-    % compose transform matrix
-    % Transform from fMRI to low res T2 star (xform), convert from low resolution to high resolution voxel size (fMRI2highResT2Star) and apply
-    % transformation from high res t2 star to whole head
-    hdr = cbiSetNiftiSform(hdr,T2StarHDR.sform44* fMRI2highResT2Star * xform);
+    % compose transformation matrix
+    % - Convert fMRI voxel indices to mm (fMRIvoxelSizeXform), this is because xform matrices output by FLIRT are in world coordinates
+    % - align to low-res T2 star (xform), 
+    % - convert from mm to high resolution T2* voxel size (inv(t2starVoxelSizeXform)) 
+    % - apply transformation from high res T2* to whole head (T2StarHDR.sform44)
+    hdr = cbiSetNiftiSform(hdr,t2Star.sform44 * inv(t2starVoxelSizeXform) * xform * fMRIvoxelSizeXform);
+%     hdr = cbiSetNiftiSform(hdr,T2StarHDR.sform44 * (t2starVoxelSizeXform \ xform) * fMRIvoxelSizeXform);
     
     hdr.sform_code = 1;
     hdr = mlrImageWriteNiftiHeader(hdr,filenames{p});
