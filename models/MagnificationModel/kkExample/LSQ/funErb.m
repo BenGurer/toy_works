e87131c9f1e10eb5ec172060cdb7a9e32e27974c
BenGurer <bengurer@googlemail.com>
2017-01-20 11:04:45 +0000

    models: removed old models

--- models/MagnificationModel/kkExample/LSQ/funErb.m
+++
@@ -1,5 +0,0 @@
-function erb = funErbGM90(f)
-% Erb (in kHz) according to Glasberg and Moore 1990 as a function of frequency, f (in kHz); 
-% f can be a vector.
-
-erb = 24.67*(4.37*f+1)/1000;
--- models/MagnificationModel/kkExample/LSQ/funRegr.m
+++
@@ -1,8 +0,0 @@
-function [M,b] = funRegr(x,y)
-
-meY = mean(y); N = length(x);
-M = (sum(x.*meY)-sum(x)*sum(meY)/N)/(sum(x.^2)-sum(x)^2/N);
-b = zeros(size(y,1),1);
-for I = 1:size(y,1)
-    b(I) = mean(y(I,:))-M*mean(x);
-end
