7e8463ee67faf40369ed3e9619838802aad8deba
Ben Gurer <bengurer@googlemail.com>
2017-10-18 17:31:03 +0100

    scripts/ scripted flatmap analysis - gradient reversal

--- scripts/hearingLossSimulation/sHL_mainScript.m
+++ scripts/hearingLossSimulation/sHL_mainScript.m
@@ -1,8 +1,10 @@
-sHL_mainScript
+%% sHL_mainScript - scripted analysis for Simulated Hearing Loss (sHL) study 2017
 % simulate hearing loss
-% hearingLossSimulation_mainScript
 
 %% run this function when returning for analysis?
+iSub = 2;
+
+thisView = getMLRView;
 
 % setup study direction (once)
 sHL_createStudyDirectory
@@ -13,9 +15,11 @@ sHL_createStudyDirectory
 % nummber of subjects
 
 % define and get subject info
-sHL_getSubjectInfo(iSub)
+% either cell for each subject or return single subject info - prob the later
+subjectInfo = get_SubjectInfo_sHL(iSub);
 % Subject ID
 % flatmap names
+
 
 sHL_organiseData(studyDirectory,subject)
 % per subject
@@ -43,32 +47,32 @@ thisView = script_importAnatomy(thisView);
 
 [thisView, pRFdata] = script_pRFAnalysis(thisView);
 
-thisView = script_flatMapAnalysis(thisView,flatmapNames);
+thisView = script_flatMapAnalysis(thisView,Info,subjectInfo);
 
 %% Create subject data storage structure
 
+%% Get analysis data from scans
+% pass on to ROI analysis to restrict
+
 %% get data from scans and groups
 % save so don't need to load again
+% change name to get_analysisData_GLM and save to analysisData.glm
+scanData = getScanData_GLM(thisView,glmInfo.analysisNames_Scans,glmInfo.analysisNames_Groups,glmInfo.groupNames);
+
 
 %% get data from ROIs
 % save so don't need to load again
-
-%% perform ROI analysis
-% save so don't need to load again
-
-%% save data
-
-thisView = getMLRView;
-%% Get analysis data from scans
-% pass on to ROI analysis to restrict
-scanData = getScanData_GLM(thisView,glmInfo.analysisNames_Scans,glmInfo.analysisNames_Groups,glmInfo.groupNames);
-
-% change name to get_analysisData_GLM and save to analysisData.glm
-
 % Set group outside of script
 roiData = script_getROIdata(thisView,scanData.scan_GLMdata,glmInfo.analysisBaseNames_Scans,Info.ROInames,glmInfo.analysisScanNum);
 
+%% perform ROI analysis
+% save so don't need to load again
 roiAnalysis = script_ROIAnalysis(roiData,glmInfo.analysisBaseNames_Scans,Info,stimInfo,plotInfo,Info.conditionRunIndex,glmInfo.analysisScanNum);
+
+%% save data
+saveName = [subjectID, 'ROIanalysis'];
+% navigate to correct dirtory
+save(saveName,roiAnalysis)
 % save data to disk
 % load later for group analysis
 
