96ded20369259cdb6716cdb8b2a3ef4a551a9b05
Ben Gurer <bengurer@googlemail.com>
2018-05-31 12:12:05 +0100

    changed function to use freesurfer name rather than subject ID

--- scripts/importPSIRasOverlay.m
+++ scripts/importPSIRasOverlay.m
@@ -9,21 +9,21 @@ function thisView = importPSIRasOverlay(thisView,Info,subjectInfo)
     %
     
     % move to anatomy folder
-    cd(fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.subjectID))
+    cd(fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.freeSurferName))
     % rename PSIR file so it works with matlab
-    eval (['!cp ', [subjectInfo.subjectID, '_PSIR_pos_-.7_thr.nii'], ' ' [subjectInfo.subjectID, '_PSIR_mrTools.nii']])
+    eval (['!cp ', [subjectInfo.freeSurferName, '_PSIR_pos_-.7_thr.nii'], ' ' [subjectInfo.freeSurferName, '_PSIR_mrTools.nii']])
     
     cd(fullfile(Info.dataDir,Info.studyDir,subjectInfo.subjectID));
     
     % import PSIR and PD
     thisView = viewSet(thisView,'newGroup','PSIR');
     thisView = viewSet(thisView,'curGroup','PSIR');
-    thisView = importTSeries(thisView,[],'defaultParams=1',['pathname=' fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.subjectID,[subjectInfo.subjectID, '_PSIR_mrTools.nii'])]);
+    thisView = importTSeries(thisView,[],'defaultParams=1',['pathname=' fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.freeSurferName,[subjectInfo.freeSurferName, '_PSIR_mrTools.nii'])]);
     thisView = newAnalysis(thisView,'dummy');
-    thisView = importOverlay(thisView,[],'defaultParams=1',['pathname=' fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.subjectID,[subjectInfo.subjectID, '_PD_smooth7.nii'])]);
-    thisView = importOverlay(thisView,[],'defaultParams=1',['pathname=' fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.subjectID,[subjectInfo.subjectID, '_PSIR_mrTools.nii'])]);
+    thisView = importOverlay(thisView,[],'defaultParams=1',['pathname=' fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.freeSurferName,[subjectInfo.freeSurferName, '_PD_smooth7.nii'])]);
+    thisView = importOverlay(thisView,[],'defaultParams=1',['pathname=' fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.freeSurferName,[subjectInfo.freeSurferName, '_PSIR_mrTools.nii'])]);
     thisView = viewSet(thisView,'overlaycolorrange',[.5 1]);
     %load PSIR as anatomy
-    thisView = loadAnat(thisView,[subjectInfo.subjectID, '_PSIR_mrTools.nii'],fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.subjectID));
+    thisView = loadAnat(thisView,[subjectInfo.freeSurferName, '_PSIR_mrTools.nii'],fullfile(Info.dataDir,'Anatomy/originals/',subjectInfo.freeSurferName));
 
 end
