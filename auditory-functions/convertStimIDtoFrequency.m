027aa39292680797a370da878dae857c7040b614
BenGurer <bengurer@googlemail.com>
2018-06-12 15:02:54 +0100

    convertStimIDtoFrequency.m this function now outputs the NERB for each stimulus as well

--- auditory-functions/convertStimIDtoFrequency.m
+++ auditory-functions/convertStimIDtoFrequency.m
@@ -1,16 +1,17 @@
-function [stimFreqs, stimFreqs_bin, stimFreqs_mv] = convertStimIDtoFrequency(lowFreqkHz,highFreqkHz,nStim)
+function [stimFreqs, stimFreqs_bin, stimFreqs_mv, stimNERBs, stimNERBs_bin, stimNERBs_mv] = convertStimIDtoFrequency(lowFreqkHz,highFreqkHz,nStim)
 
 stimNERBs = funNErb(lowFreqkHz):(funNErb(highFreqkHz)-funNErb(lowFreqkHz))/(nStim-1):funNErb(highFreqkHz);
 
 stimFreqs = funInvNErb(stimNERBs);
 
 % bin stimulus
-
-stimFreqs_bin = [];
 binSize = 4;
+stimFreqs_bin = zeros(1,length(stimNERBs)/binSize);
+stimNERBs_bin = zeros(1,length(stimNERBs)/binSize);
 c = 1;
 for i = 1:length(stimNERBs)/binSize
 stimFreqs_bin(i) = funInvNErb(mean(stimNERBs(c:c + (binSize-1))));
+stimNERBs_bin(i) = mean(stimNERBs(c:c + (binSize-1)));
 c = c + binSize;
 end
 
@@ -23,9 +24,11 @@ if isreal(windowAvSize) && rem(windowAvSize,1)==0
     
 loopLength = length(stimNERBs) - windowAvSize;
 stimFreqs_mv = zeros(1,loopLength);
+stimNERBs_mv = zeros(1,loopLength);
 
 for i = 1:loopLength
 stimFreqs_mv(i) = funInvNErb(mean(stimNERBs(i:i+windowAvSize-1)));
+stimNERBs_mv(i) = mean(stimNERBs(i:i+windowAvSize-1));
 end
 
 else
